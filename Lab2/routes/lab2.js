7e40e41d35ae0110b4b15c95d8f910bb7206937c
DavidWiafe <wiafe.d@hotmail.com>
2019-09-24 21:06:25 -0400

    first commit

---
+++ Lab2/routes/lab2.js
@@ -0,0 +1,85 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+
+    var value1;
+
+    var value2;
+
+    var answer;
+
+    // res.render('index', { title: 'Express' });
+    if (req.query.method) {
+       /* if (req.query.method != 'add' || req.query.method != 'subtract'
+            || req.query.method != 'multiply' || req.query.method != 'divide' ) {
+
+            res.send('Error your method ' + req.query.method + ' is not supported');
+        } */
+        if (req.query.method == 'add') {
+            if (req.query.x && req.query.y) {
+
+                value1 = parseFloat(req.query.x);
+
+                value2 = parseFloat(req.query.y);
+
+                answer = value1 + value2;
+
+                res.send('' + String(value1) + ' + ' + String(value2) + ' = ' + String(answer));
+
+            }
+        } else if (req.query.method == 'subtract') {
+
+            if (req.query.x && req.query.y) {
+
+                value1 = parseFloat(req.query.x);
+
+                value2 = parseFloat(req.query.y);
+
+                answer = value1 - value2;
+
+                res.send('' + String(value1) + ' - ' + String(value2) + ' = ' + String(answer));
+
+            }
+        } else if (req.query.method == 'multiply') {
+
+            if (req.query.x && req.query.y) {
+
+                value1 = parseFloat(req.query.x);
+
+                value2 = parseFloat(req.query.y);
+
+                answer = value1 * value2;
+
+                res.send('' + String(value1) + ' x ' + String(value2) + ' = ' + String(answer));
+
+            }
+
+        } else if (req.query.method == 'divide') {
+
+            if (req.query.x && req.query.y) {
+
+                value1 = parseFloat(req.query.x);
+
+                value2 = parseFloat(req.query.y);
+
+                answer = value1 / value2;
+
+                res.send('' + String(value1) + ' / ' + String(value2) + ' = ' + String(answer));
+
+            }
+
+        } else {
+            res.send('Error your method ' + req.query.method + ' is not supported');
+        }
+
+        } else {
+        res.send('Lab2 screen');
+    }
+});
+
+module.exports = router;
